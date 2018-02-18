82c9f8e55c9f355ace6375d02bfc7053368d98b3
Tortera1999 <nikhilnarendra1999@gmail.com>
2018-02-18 17:48:36 -0500

    Added files

---
+++ Photo Map/PhotoMapViewController.swift
@@ -0,0 +1,143 @@
+//
+//  PhotoMapViewController.swift
+//  Photo Map
+//
+//  Created by Nicholas Aiwazian on 10/15/15.
+//  Copyright © 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate, MKMapViewDelegate,UINavigationControllerDelegate, LocationsViewControllerDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    var currentImage: UIImage?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667),
+                                              MKCoordinateSpanMake(0.1, 0.1))
+        mapView.setRegion(sfRegion, animated: false)
+        
+        mapView.delegate = self
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func CameraButton(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera is available 📸")
+            vc.sourceType = UIImagePickerControllerSourceType.camera
+        } else {
+            print("Camera 🚫 available so we will use photo library instead")
+            vc.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        }
+        
+        self.present(vc, animated: true, completion: nil)
+        
+        
+        
+        
+        
+    }
+    
+
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        let size = CGSize(width: 288, height: 288)
+        let newImage = resize(image: editedImage, newSize: size)
+        
+        currentImage = newImage
+        
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+        performSegue(withIdentifier: "tagSegue", sender: nil)
+    }
+    
+    func resize(image: UIImage, newSize: CGSize) -> UIImage {
+        let resizeImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        resizeImageView.contentMode = UIViewContentMode.scaleAspectFill
+        resizeImageView.image = image
+        
+        UIGraphicsBeginImageContext(resizeImageView.frame.size)
+        resizeImageView.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
+
+    func locationsPickedLocation(controller: LocationsViewController, latitude: NSNumber, longitude: NSNumber) {
+        //print("HIIII");
+        let locationCoordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(latitude), longitude: CLLocationDegrees(longitude));
+        
+        let annotation = PhotoAnnotation()
+        annotation.coordinate = locationCoordinate
+        annotation.photo = currentImage
+        mapView.addAnnotation(annotation)
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseID = "myAnnotationView"
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseID)
+        if (annotationView == nil) {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseID)
+            annotationView!.canShowCallout = true
+            annotationView!.leftCalloutAccessoryView = UIImageView(frame: CGRect(x:0, y:0, width: 50, height:50))
+        }
+        
+        
+        let resizeRenderImageView = UIImageView(frame: CGRect(x:0, y:0, width: 45, height:45))
+        resizeRenderImageView.layer.borderColor = UIColor.white.cgColor
+        resizeRenderImageView.layer.borderWidth = 3.0
+        resizeRenderImageView.contentMode = UIViewContentMode.scaleAspectFill
+        resizeRenderImageView.image = (annotation as? PhotoAnnotation)?.photo
+        
+        UIGraphicsBeginImageContext(resizeRenderImageView.frame.size)
+        resizeRenderImageView.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let thumbnail = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        let imageView = annotationView?.leftCalloutAccessoryView as! UIImageView
+        imageView.image = thumbnail
+        
+        annotationView?.image = thumbnail
+        
+        
+        return annotationView
+    }
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        
+        if(segue.identifier == "tagSegue"){
+            let destination = segue.destination as! LocationsViewController
+            destination.delegate = self
+        }
+    }
+    
+
+}
